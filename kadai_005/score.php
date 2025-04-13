bbc9590afe95911cc997f41eb95ddaae0c6fb62a
KazuT123 <kazu.tamu0817@gmail.com>
2025-04-13 12:27:09 +0900

    add untracked files

---
+++ kadai_005/score.php
@@ -0,0 +1,12 @@
+<?php
+$total = 0;
+
+$scores = [80, 60, 55, 40, 100, 25, 80, 95, 30, 60];
+for ($i = 0; $i < 10; $i++) {
+  $varName = "score" . ($i + 1);
+  $$varName = $scores[$i];
+  $total += $$varName;
+}
+$average=$total/10;
+echo $average;
+?>
---
+++ kadai_007/associative-array.php
@@ -0,0 +1,9 @@
+<?php
+$items = [
+  'name' => 'onion',
+  'price' => 200,
+  'weight' => 160
+
+];
+print_r($items);
+?>
